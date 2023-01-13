a31a85047ff4bdcf8b4468e59d1093b70184e5f9
Taise228 <yamamototaisei534@gmail.com>
2023-01-13 21:24:37 +0900

    hide secret key

--- TFC_PB/TFC/settings.py
+++ TFC_PB/TFC/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-38rb*t97@^c^vkb1_&e(l986+-=8m_dmq*kaxu1+p+5c=f9w2!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
